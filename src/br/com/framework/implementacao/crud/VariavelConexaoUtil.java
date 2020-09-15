03ee6cf9390a6ed294458804d63bd3e6922b1e53
SergioP <SergioP@Suporte1Residencia>
2020-09-15 20:28:20 -0300

    Comit do meu projeto

---
+++ src/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
@@ -0,0 +1,14 @@
+package br.com.framework.implementacao.crud;
+
+/**
+ * Nome do caminho do JNDI datasource do Tomcat
+ * @author SergioP
+ *
+ */
+public class VariavelConexaoUtil {
+
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+	
+}
+
+
